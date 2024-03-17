670305e850e113e7a6b678d613c65048209475fb
alamin khan <alaminuixd@gmail.com>
2024-03-18 04:52:36 +0600

    18 March 2024 end day commit

--- src/functions/callback/arrrayCallback.js
+++ src/functions/callback/arrrayCallback.js
@@ -1,5 +1,7 @@
 const letterArray = ["a", "b", "c", "d", "e"];
 const arrayReduce = (arr, callback) => {
   const reducedArray = [];
-  for (let i = 0; i < reducedArray.length; i++) {}
+  for (let i = 0; i < reducedArray.length; i++) {
+    return reducedArray.push(callback());
+  }
 };
