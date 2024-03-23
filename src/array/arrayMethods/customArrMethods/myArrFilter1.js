333f8ff0861198c57622e1a90d398b8a75f2c522
alamin khan <alaminuixd@gmail.com>
2024-03-24 02:29:08 +0600

    renamed custom array functions and updated and added new custom array functions

---
+++ src/array/arrayMethods/customArrMethods/myArrFilter1.js
@@ -0,0 +1,43 @@
+// Function to filter array elements based on a callback function
+function filterByIndex(arr, callback) {
+  const filtered = [];
+  for (let i = 0; i < arr.length; i++) {
+    // ( arr[i] OR each "item" ) need to pass test in "callback"
+    if (callback(arr[i])) {
+      // push all items one by one, passed in "callback, in the "filtered" array.
+      filtered.push(arr[i]);
+    }
+  }
+  return filtered;
+}
+
+// Functions for checking if a string's first character matches a given character
+function incFirstChr(item, chr) {
+  return item.toLowerCase()[0] === chr.toLowerCase();
+}
+function excFirstChr(item, chr) {
+  return item.toLowerCase()[0] !== chr.toLowerCase();
+}
+// Functions for checking if a string's last character matches a given character
+function incLastChr(item, chr) {
+  return item.toLowerCase().slice(-1) === chr.toLowerCase();
+}
+function excLastChr(item, chr) {
+  return item.toLowerCase().slice(-1) !== chr.toLowerCase();
+}
+
+const names = ["sina", "Sam", "Qoli", "Ben", "Zod", "Quantin", "Ala"];
+
+// Filter names where the first character is 'q'
+const incFirstQ = filterByIndex(names, (item) => incFirstChr(item, "q"));
+// Filter names where the first character is not 'q'
+const excFirstQ = filterByIndex(names, (item) => excFirstChr(item, "q"));
+// Filter names where the last character is 'a'
+const incLastA = filterByIndex(names, (item) => incLastChr(item, "a"));
+// Filter names where the last character is not 'a'
+const excLastA = filterByIndex(names, (item) => excLastChr(item, "a"));
+
+console.log(incFirstQ);
+console.log(excFirstQ);
+console.log(incLastA);
+console.log(excLastA);
--- src/destructuring/objDestructuring/objDest1.js
+++ src/destructuring/objDestructuring/objDest1.js
@@ -1,5 +1,16 @@
+// destructing directly to the object
 const { fullName, age } = {
   fullName: "John Doe",
   age: 30,
 };
 console.log(`${fullName} ${age}`);
+
+/*
+  destructing with pre declared object
+*/
+const obj1 = {
+  name: "Al Amin Khan",
+  father: "Syed Ahmed Khan",
+  mother: "Mamataj Begum",
+  children: 2,
+};
