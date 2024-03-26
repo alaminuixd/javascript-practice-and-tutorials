b3a4f980f3c598e78711d4b32cc2edb939b08edc
alamin khan <alaminuixd@gmail.com>
2024-03-27 02:54:15 +0600

    getter setter update 2

--- src/objects/getSet/getSet-04.js
+++ src/objects/getSet/getSet-04.js
@@ -1,5 +1,5 @@
-// A setter can change "properties value" inside an Object permenantly only when assigning these "values" in the "Setter"
-// The following properties "fName" & "lName" value inside the "obj" will not change permenantly as we did not change/ reassign these values in the setter.
+// A setter can change properties' values inside an object permanently only when assigning these values in the setter.
+// The following properties, "fName" & "lName" inside "obj", will not change permanently as we did not reassign these values in the setter.
 const obj = {
   fName: "Al Amin",
   lName: "khan",
@@ -7,7 +7,7 @@ const obj = {
     return `${this.fName} ${this.lName}`;
   },
   set fullName(val) {
-    // Here we only returned the setter "val" but did not assigned it to the "obj" properties value. So this will retaren only the value we will assign temporaryly.
+    // Here we only return the setter "val" but do not assign it to the "obj" properties' values. So, this will only temporarily hold the assigned value.
     return val;
   },
 };
@@ -22,8 +22,8 @@ let res2 = (obj.fullName = "Ruhul Amin Khan");
 console.log(res2); // result: Ruhul Amin Khan
 // but if we print it directly it will return the default value "Al Amin"
 console.log(obj.fullName); // result: Al Amin Khan
-// We can see that the value is not changed parmenantly
-// We can change the object propertys values assigning them to the setter parameter as follow
+// We can see that the value is not changed permanently
+// We can change the object properties' values by assigning them to the setter parameter as follows:
 const obj2 = {
   fName: "Al Amin",
   lName: "khan",
@@ -31,24 +31,24 @@ const obj2 = {
     return `${this.fName} ${this.lName}`;
   },
   set fullName(val) {
-    // first we will make an array from the values that we will add later using "join()"
-    valArray = val.split(" ");
+    // First, we will make an array from the values that we will add later using "join()".
+    const valArray = val.split(" ");
     this.fName = valArray[0];
     this.lName = valArray[1];
-    // We don't have to return here anything as we've already returned "fName" & "lName" in the getter.
-    // Anything we will add to the setter "fullName" will be splited into an Array values
+    // We don't have to return anything here as we've already returned "fName" & "lName" in the getter.
+    // Anything we will add to the setter "fullName" will be split into an array of values.
     /* 
-    If we set obj2.fullName = "Abrar Syed Khan", splite will divide them in an Array with 3 elements as follow:
-    ["Abrar", "Syed", "Khan"]
-    As we assign only [0] & [1] index value to the "fName" and "lName" accordingly, we will only get these two values from the setter and "Khan" will be ommited.
-    We will learn how can we add the remaining value in [1] index in the next object
-    */
+      If we set obj2.fullName = "Abrar Syed Khan", split will divide them into an array with 3 elements as follows:
+      ["Abrar", "Syed", "Khan"]
+      As we assign only [0] & [1] index values to "fName" and "lName" accordingly, we will only get these two values from the setter, and "Khan" will be omitted.
+      We will learn how we can add the remaining value in [1] index in the next object.
+      */
   },
 };
 console.log(obj2.fullName); // result: Al Amin Khan
 obj2.fullName = "Abrar Syed Khan";
-// We will only get "Abrar Syed" where "Abrar" is index[0] and "Syed" is index[1]
-// The "khan" is assigned to the index[2] by split method and we only assigned [0] & [1] indexes to the "fName" and "lName".
+// We will only get "Abrar Syed" where "Abrar" is index[0] and "Syed" is index[1].
+// The "khan" is assigned to index[2] by the split method, and we only assigned [0] & [1] indexes to "fName" and "lName".
 console.log(obj2.fullName); // result: Abrar Syed.
 
 // Let's solve the problem
@@ -59,13 +59,31 @@ const obj3 = {
     return `${this.fName} ${this.lName}`;
   },
   set fullName(val) {
-    valArray = val.split(" ");
+    const valArray = val.split(" ");
     this.fName = valArray[0];
     /* 
-    As we've already assigned index[0] to the fName we will remove index[0] and add rest to the lName using slice(1).join().
-    slice(1) will remove the index[0] and join() will merge rest to a single value.
-    */
+      As we've already assigned index[0] to fName, we will remove index[0] and add the rest to lName using slice(1).join().
+      slice(1) will remove index[0], and join() will merge the rest into a single value with a space.
+      */
     this.lName = valArray.slice(1).join("");
+  },
+};
+console.log(obj3.fullName); // result: Al Amin Khan
+obj3.fullName = "Abrar Syed Khan";
+console.log(obj3.fullName); // result: Abrar Syed Khan
+
+// We can also do this using the spread operator
+const obj4 = {
+  fName: "Al Amin",
+  lName: "khan",
+  get fullName() {
+    return `${this.fName} ${this.lName}`;
+  },
+  set fullName(val) {
+    // "first" represents the first element (index[0]) of the split value, while "...last" represents the remaining elements as an array.
+    const [first, ...last] = val.split(" ");
+    this.fName = first;
+    this.lName = [...last].join(" ");
   },
 };
 console.log(obj3.fullName); // result: Al Amin Khan
