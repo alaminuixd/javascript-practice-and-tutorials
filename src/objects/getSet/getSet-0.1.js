36c257ec6437d8faa4505dece03cda73aa2716bc
alamin khan <alaminuixd@gmail.com>
2024-04-25 13:08:34 +0600

    getter and setter

--- src/objects/getSet/getSet-0.1.js
+++ src/objects/getSet/getSet-0.1.js
@@ -10,15 +10,15 @@ const person = {
   greeting: "",
 };
 
-// 2) accessor properties are "methods" to "get" and "set" the property of an object. These properties are made with "get" and "set"
+// 2) "accessor" properties are "methods" to "get" and "set" the property of an object. These properties are made with "get" and "set"
 const person2 = {
   // Data property
   greeting: "",
-  // accessor property
+  // "set" is accessor property
   set firstName(value) {
     this.greeting = value + " is my first name.";
   },
-  // accessor property
+  // "get" is also accessor property
   get firstName() {
     return this.greeting + "and don't call me.";
   },
