08f5b81e4090b686e2faf793ad7f037597b1c255
alamin khan <alaminuixd@gmail.com>
2024-05-18 19:33:36 +0600

    getSet.js update

--- src/objects/getSet/getSet-0.1.js
+++ src/objects/getSet/getSet-0.1.js
@@ -7,20 +7,21 @@ JavaScript "Objects" consist of two property types:
 // 1) data properties are properties that associate "data" with an "object".
 const person = {
   // Data property
-  greeting: "",
+  pName: "",
 };
 
 // 2) "accessor" properties are "methods" to "get" and "set" the property of an object. These properties are made with "get" and "set"
 const person2 = {
   // Data property
-  greeting: "",
-  // "get" is also accessor property
+  pName: "",
+  // "get" is an accessor property
   get firstName() {
-    return this.greeting + "and don't call me.";
+    return this.pName + "and don't call me.";
   },
-  // "set" is accessor property
+  // "set" is also an accessor property
   set firstName(value) {
-    this.greeting = value + " is my first name.";
+    console.log(value);
+    this.pName = value + " is my first name.";
   },
 };
 
