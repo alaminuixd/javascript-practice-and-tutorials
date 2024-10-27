1dc8872b6fb5ebd9e88aafcf61ee892ca1ca54b1
alamin khan <alaminuixd@gmail.com>
2024-10-27 14:25:46 +0600

    max-num exprement

---
+++ src/experiments/max-num.js
@@ -0,0 +1,14 @@
+function test(...nums) {
+  let max = -Infinity;
+  if (Array.isArray(nums[0])) {
+    nums = nums[0];
+  }
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] > max) {
+      max = nums[i];
+    }
+  }
+  return max;
+}
+const resWithArr = test([5, 2, 8, 4]); // Works with array input
+const resWithNum = test(9, 3, 1, 4); // Works with multiple numbers
