1e7f5cc4f85fe23eead3213ea62781335c23bf6f
alamin khan <alaminuixd@gmail.com>
2025-02-07 20:27:31 +0600

    recursion return

--- src/functions/recursion/05-recursion-return.js
+++ src/functions/recursion/05-recursion-return.js
@@ -5,3 +5,17 @@ function fetchWater(attempt = 0) {
 }
 const result = fetchWater();
 console.log(result);
+
+// res
+function parent() {
+  let num = 0; // Initialize `num` to 0
+  function inner() {
+    if (num >= 10) return num; // Base case: when num reaches 10, return num and stop recursion
+    num++; // Increment `num` by 1
+    return inner(); // Recursively call inner until base case is met
+  }
+  // Start the recursion by invoking `inner` and return its result
+  return inner(); // When the recursion finishes, the final value of `num` is returned
+}
+
+console.log(parent()); // Output: 10
--- src/functions/returning.js
+++ src/functions/returning.js
@@ -1,4 +1,31 @@
-/* Returning value */
 /*
+ Returning value
 When a method returns a value, it's meant to give you something that you can use or store. 
+A function always return a single value.
+When we need to return more than one value we can enclose them in an object or array
  */
+
+// Function a returns nothing, so it implicitly returns 'undefined'
+function a() {
+  return; // 'undefined' is returned by default when there is no explicit return value
+}
+
+a(); // The output of a() is 'undefined'
+
+// Function b returns the result of adding 1 and 2
+function b() {
+  return 1 + 2; // Returns the sum of 1 and 2, which is 3
+}
+
+b(); // The output of b() is 3
+
+// Function c returns an array containing two sums
+function c() {
+  let a = 1;
+  let b = 2;
+  let c = 3;
+  // The array contains two sums: a + b and b + c
+  return [a + b, b + c]; // Returns an array with values [3, 5]
+}
+
+c(); // The output of c() is [3, 5], which is an array containing the sums
